f42f17f762ee365bb63e62e224314a4b7227c107
Mike Purvis <mikedotexe@gmail.com>
2020-04-13 04:26:07 -0700

    change dep nearlib to near-api-js (#67)
    * change dep nearlib to near-api-js
    
    * rename nearAPIJs to nearAPI

--- src/index.js
+++ src/index.js
@@ -2,28 +2,28 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import getConfig from './config.js'
-import * as nearlib from 'nearlib'
+import * as nearAPI from 'near-api-js'
 
 // Initializing contract
 async function initContract () {
   const nearConfig = getConfig(process.env.NODE_ENV || 'development')
 
   // Initializing connection to the NEAR DevNet
-  const near = await nearlib.connect({
+  const near = await nearAPI.connect({
     deps: {
-      keyStore: new nearlib.keyStores.BrowserLocalStorageKeyStore()
+      keyStore: new nearAPI.keyStores.BrowserLocalStorageKeyStore()
     },
     ...nearConfig
   })
 
   // Needed to access wallet
-  const walletConnection = new nearlib.WalletConnection(near)
+  const walletConnection = new nearAPI.WalletConnection(near)
 
   // Get Account ID – if still unauthorized, it's an empty string
   const accountId = walletConnection.getAccountId()
 
   // Initializing our contract APIs by contract name and configuration
-  const contract = await new nearlib.Contract(walletConnection.account(), nearConfig.contractName, {
+  const contract = await new nearAPI.Contract(walletConnection.account(), nearConfig.contractName, {
     // View methods are read-only – they don't modify the state, but usually return some value
     viewMethods: ['getMessages'],
     // Change methods can modify the state, but you don't receive the returned value when called
