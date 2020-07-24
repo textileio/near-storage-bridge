784d4d1632e0659ddea50b48aaee6e2414ecf347
Josh Ford <thisisjoshford@gmail.com>
2020-07-24 14:13:47 -0700

    domain change

--- src/config.js
+++ src/config.js
@@ -17,7 +17,7 @@ function getConfig (env) {
         networkId: 'default',
         nodeUrl: 'https://rpc.testnet.near.org',
         contractName: CONTRACT_NAME,
-        walletUrl: 'https://wallet.testnet.nearprotocol.org',
+        walletUrl: 'https://wallet.testnet.nearprotocol.com',
         helperUrl: 'https://helper.testnet.near.org'
       }
     case 'betanet':
