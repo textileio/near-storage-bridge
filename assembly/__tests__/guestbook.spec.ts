25dc4103fa2f72cdb5816d140f76e03f5e9807d4
Josh Ford <thisisjoshford@gmail.com>
2020-11-03 21:52:42 -0800

    attached deposit test passing

--- assembly/__tests__/guestbook.spec.ts
+++ assembly/__tests__/guestbook.spec.ts
@@ -8,28 +8,43 @@ function createMessage (text: string): PostedMessage {
 
 const message = createMessage('hello world')
 
-beforeEach(() => {
-  VMContext.setAttached_deposit(u128.fromString('0'))
-  VMContext.setAccount_balance(u128.fromString('0'))
-})
-
 describe('message tests', () => {
+  beforeEach(() => {
+    addMessage('hello world')
+  })
+
+  afterEach(() => {
+    while (messages.length > 0) {
+      messages.pop()
+    }
+  })
+
   it('adds a message', () => {
-    addMessage(hello)
-    expect(messages.length).toBe(1, 'should only contain one message')
-    expect(messages[0]).toStrictEqual(message, 'message should be "hello world"')
+    expect(messages.length).toBe(
+      1,
+      'should only contain one message'
+    )
+    expect(messages[0]).toStrictEqual(
+      message,
+      'message should be "hello world"'
+    )
   })
 
   it('retrieves messages', () => {
-    addMessage(hello)
     const messagesArr = getMessages()
-    expect(messagesArr.length).toBe(1, 'should be one message')
-    expect(messagesArr).toIncludeEqual(message, 'messages should include:\n' + message.toJSON())
+    expect(messagesArr.length).toBe(
+      1,
+      'should be one message'
+    )
+    expect(messagesArr).toIncludeEqual(
+      message,
+      'messages should include:\n' + message.toJSON()
+    )
+    log('GETS MESSAGES TEST:')
     log(messagesArr[0])
   })
 
-  it('only show the last ten messages', () => {
-    addMessage(hello)
+  it('only show the last 10 messages', () => {
     const newMessages: PostedMessage[] = []
     for (let i: i32 = 0; i < 10; i++) {
       const text = 'message #' + i.toString()
@@ -37,21 +52,36 @@ describe('message tests', () => {
       addMessage(text)
     }
     const messages = getMessages()
+    log('SHOWS LAST 10 MESSAGES TEST:')
     log(messages.slice(7, 10))
-    expect(messages).toStrictEqual(newMessages, 'should be the last ten messages')
-    expect(messages).not.toIncludeEqual(message, "shouldn't contain the first element")
+    expect(messages).toStrictEqual(
+      newMessages,
+      'should be the last ten messages'
+    )
+    expect(messages).not.toIncludeEqual(
+      message,
+      'shouldn\'t contain the first element'
+    )
   })
 })
 
-describe('attached deposit tests', () => {
-  it('attaches a deposit', () => {
-    log('Initial account balance:')
-    log(Context.accountBalance.toString())
-    VMContext.setAttached_deposit(u128.from('10000000000000000000000'))
-    addMessage(hello)
-    log('Account balance after deposit:')
-    log(Context.accountBalance.toString())
-    log(messages[0])
-    expect(Context.accountBalance.toString()).toStrictEqual('10000000000000000000000', 'balance should be 10000000000000000000000')
+describe('attached deposit test', () => {
+  beforeEach(() => {
+    VMContext.setAttached_deposit(u128.fromString('0'))
+    VMContext.setAccount_balance(u128.fromString('0'))
+    addMessage('hello world')
+  })
+
+  it('attaches a deposit to a contract call', () => {
+    log('ATTACHED DEPOSIT TEST:')
+    log('Initial account balance: ' + Context.accountBalance.toString())
+
+    VMContext.setAttached_deposit(u128.from('10'))
+    log('Attached deposit: 10')
+    log('Account balance after deposit: ' + Context.accountBalance.toString())
+
+    expect(Context.accountBalance.toString()).toStrictEqual('10',
+      'balance should be 10'
+    )
   })
 })
