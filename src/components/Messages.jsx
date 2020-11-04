5b8a5c599ecfcf188ac033b9467a2e4768d408f0
Josh Ford <thisisjoshford@gmail.com>
2020-11-03 22:43:02 -0800

    added components

---
+++ src/components/Messages.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Messages ({ messages }) {
+  return (
+    <>
+      <h2>Messages</h2>
+      {messages.map((message, i) =>
+        // TODO: format as cards, add timestamp
+        <p key={i} className={message.premium ? 'is-premium' : ''}>
+          { console.log(message)}
+          <strong>{message.sender}</strong>:<br/>
+          {message.text}
+        </p>
+      )}
+    </>
+  )
+}
+
+Messages.propTypes = {
+  messages: PropTypes.array
+}
