10dabd50bba8f4f9302539a0d08b76cdb96b8c48
Josh Ford <thisisjoshford@gmail.com>
2020-11-25 22:46:44 -0800

    update deploy.yml and eslintrc.yml

--- assembly/main.ts
+++ assembly/main.ts
@@ -1,4 +1,4 @@
-import { PostedMessage, messages } from "./model";
+import { PostedMessage, messages } from './model';
 
 // --- contract code goes below
 
@@ -20,12 +20,12 @@ export function addMessage(text: string): void {
 /**
  * Returns an array of last N messages.\
  * NOTE: This is a view method. Which means it should NOT modify the state.
- */ 
+ */
 export function getMessages(): PostedMessage[] {
   const numMessages = min(MESSAGE_LIMIT, messages.length);
   const startIndex = messages.length - numMessages;
   const result = new Array<PostedMessage>(numMessages);
-  for (let i = 0; i < numMessages; i++) {
+  for(let i = 0; i < numMessages; i++) {
     result[i] = messages[i + startIndex];
   }
   return result;
